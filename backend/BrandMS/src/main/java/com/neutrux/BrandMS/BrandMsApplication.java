653391e2e1a4dd5a8a923b373c47c1bcba8aaef7
Saurabh Akhade <saurabhakhade1027@gmail.com>
2025-01-29 00:13:26 +0530

    Projects creation
    created Frontend (REACT) and Backend (SPRING) projects

---
+++ backend/BrandMS/src/main/java/com/neutrux/BrandMS/BrandMsApplication.java
@@ -0,0 +1,13 @@
+package com.neutrux.BrandMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandMsApplication.class, args);
+	}
+
+}
---
+++ backend/BrandMS/src/test/java/com/neutrux/BrandMS/BrandMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutrux.BrandMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
